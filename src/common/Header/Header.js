bbb1726fd2e45127799bfbd303b201acc4c8f926
komal1204 <kattykomal1@gmail.com>
2019-02-19 10:36:59 +0530

    Added spinning logo

--- src/common/Header/Header.js
+++ src/common/Header/Header.js
@@ -1,16 +1,20 @@
 import React , {Component} from 'react';
 import './Header.css'
 import Button from '@material-ui/core/Button';
+import logo from '../../assets/logo.svg'
 
 class Header extends Component{
     render(){
         return(
-            <div className="header">
+            <div>
+            <header className="header">
+            <img src ={logo} className="app-logo" alt="logo"/>
             <div className="login-button">
                 <Button variant="contained" colour="default">
                     Login
                 </Button>
              </div>
+             </header>
             </div>
         )
     }
