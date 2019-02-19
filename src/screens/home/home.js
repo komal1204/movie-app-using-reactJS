f8e8df17b9117acd93106142ebe1facd04f6b8fb
komal1204 <kattykomal1@gmail.com>
2019-02-19 09:56:46 +0530

    Added Login Button to Header

--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
-import './home.css';
-
+import './Home.css';
+import Header from '../../common/Header/Header.js';
 class Home extends Component{
     render(){
         return(
-            <div></div>
+            <div>
+                <Header/>
+            </div>
         )
     }
 }
