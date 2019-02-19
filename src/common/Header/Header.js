e393f2cea7850a4b9900944967d89ac8643b35b4
komal1204 <kattykomal1@gmail.com>
2019-02-19 16:00:44 +0530

    Added required field validation in username control in login form

--- src/common/Header/Header.js
+++ src/common/Header/Header.js
@@ -10,6 +10,7 @@ import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import Input from '@material-ui/core/Input';
 import PropTypes from 'prop-types';
+import FormHelperText from '@material-ui/core/FormHelperText';
 const customStyles = {
     content: {
         top: '50%',
@@ -36,7 +37,9 @@ class Header extends Component {
         super();
         this.state = {
             modalIsOpen: false,
-            value: 0
+            value: 0,
+            usernameRequired: "dispNone",
+            username: ""
         };
     }
 
@@ -49,6 +52,13 @@ class Header extends Component {
     }
     tabChangeHandler = (event, value) => {
         this.setState({ value });
+    }
+    loginClickHandler = () => {
+        this.state.username === "" ? this.setState({ usernameRequired: "dispBlock" }) : this.setState({ usernameRequired: "dispNone" });
+    }
+
+    inputUsernameChangeHandler = (e) => {
+        this.setState({ username: e.target.value });
     }
     render() {
         return (
@@ -76,13 +86,16 @@ class Header extends Component {
                     <TabContainer>
                         <FormControl required>
                             <InputLabel htmlFor="username">Username</InputLabel>
-                            <Input id="username" type ="text"></Input>
+                            <Input id="username" type="text" username={this.state.username} onChange={this.inputUsernameChangeHandler} />
+                                <FormHelperText className={this.state.usernameRequired}>
+                                    <span className="red">required</span>
+                                </FormHelperText>
                         </FormControl><br/><br/>
                         <FormControl required>
                             <InputLabel htmlFor="password">Password</InputLabel>
                             <Input id="password" type ="password"></Input>
                         </FormControl><br/><br/>
-                        <Button variant="contained" color="primary"> Login</Button>
+                        <Button variant="contained" color="primary" onClick={this.loginClickHandler}>LOGIN</Button>
                     </TabContainer>}
                 </Modal>
             </div>
