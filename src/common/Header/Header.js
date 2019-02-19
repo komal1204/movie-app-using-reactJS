70d7943fdd8058a6946e7cb0cb4d733075ab1c12
komal1204 <kattykomal1@gmail.com>
2019-02-19 11:53:40 +0530

    Added Tabs inside modal for Login Button

--- src/common/Header/Header.js
+++ src/common/Header/Header.js
@@ -1,41 +1,51 @@
-import React , {Component} from 'react';
+import React, { Component } from 'react';
 import './Header.css'
 import Button from '@material-ui/core/Button';
 import logo from '../../assets/logo.svg';
 import Modal from 'react-modal';
-
-class Header extends Component{
-    constructor(){
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+class Header extends Component {
+    constructor() {
         super();
-        this.state={
-            modalIsOpen:false
+        this.state = {
+            modalIsOpen: false,
+            value: 0
         };
     }
 
-    openModalHandler = () =>{
-        this.setState({modalIsOpen: true})
-        
-    }
-    closeModalHandler=()=>{
-        this.setState({modalIsOpen:false})
-    }
-    render(){
-        return(
-            <div>
-            <header className="header">
-            <img src ={logo} className="app-logo" alt="logo"/>
-            <div className="login-button">
-                <Button variant="contained" colour="default"
-                onClick={this.openModalHandler}>
-                    Login
-                </Button>
-             </div>
-             </header>
-             <Modal ariaHideApp={false} isOpen = {this.state.modalIsOpen}
-             contentLabel = "Login"
-             onRequestClose = {this.closeModalHandler}>
+    openModalHandler = () => {
+        this.setState({ modalIsOpen: true })
 
-             </Modal>
+    }
+    closeModalHandler = () => {
+        this.setState({ modalIsOpen: false })
+    }
+    tabChangeHandler=(event,value)=>{
+        this.setState({value});
+    }
+    render() {
+        return (
+            <div>
+                <header className="header">
+                    <img src={logo} className="app-logo" alt="logo" />
+                    <div className="login-button">
+                        <Button variant="contained" colour="default"
+                            onClick={this.openModalHandler}>
+                            Login
+                        </Button>
+                    </div>
+                </header>
+                <Modal ariaHideApp={false} isOpen={this.state.modalIsOpen}
+                    contentLabel="Login"
+                    onRequestClose={this.closeModalHandler}>
+                    <Tabs value={this.state.value}
+                    onChange={this.tabChangeHandler}>
+                        <Tab label="login" />
+                        <Tab label="Register" />
+
+                    </Tabs>
+                </Modal>
             </div>
         )
     }
